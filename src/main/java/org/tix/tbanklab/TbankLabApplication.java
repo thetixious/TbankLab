53d76da43ccae1726e36e3cea4162053042ecf32
Arkadiy <ri.arkadiy@gmail.com>
2024-08-03 18:45:38 +0800

    init commit

---
+++ src/main/java/org/tix/tbanklab/TbankLabApplication.java
@@ -0,0 +1,13 @@
+package org.tix.tbanklab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbankLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbankLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tix/tbanklab/TbankLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tix.tbanklab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbankLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
